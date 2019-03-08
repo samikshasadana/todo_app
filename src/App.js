8ae5a85a432418385a2a67371b759dad74cbb03d
samiksha <sadanasamiksha@gmail.com>
2019-03-08 14:27:43 +0530

    moved AddTodo to components folder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Todo from './Todo';
-import AddTodo from './AddTodo';
+import AddTodo from './components/AddTodo';
 
 class App extends Component {
   state = {
